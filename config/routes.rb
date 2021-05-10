dbdc94d3b40fc70fcbbbfbe7cf3ea83ada3f3c0f
michelleroos <hello.michelleroos@gmail.com>
2021-05-10 16:54:11 -0400

    created ajax requests in api_util

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # == Route Map
 #
-#                    Prefix Verb   URI Pattern              Controller#Action
-#                      root GET    /                        static_pages#root
-#                 api_users POST   /api/users(.:format)     api/users#create {:format=>:json}
-#               api_session DELETE /api/session(.:format)   api/sessions#destroy {:format=>:json}
-#                           POST   /api/session(.:format)   api/sessions#create {:format=>:json}
+#      Prefix Verb   URI Pattern              Controller#Action
+#        root GET    /                        static_pages#root
+#   api_users POST   /api/users(.:format)     api/users#create {:format=>:json}
+# api_session DELETE /api/session(.:format)   api/sessions#destroy {:format=>:json}
+#             POST   /api/session(.:format)   api/sessions#create {:format=>:json}
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
