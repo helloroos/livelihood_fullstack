c5e49f44862e9ea709d22eecf2eebc0d5c6c6c3c
michelleroos <hello.michelleroos@gmail.com>
2021-05-10 14:21:49 -0400

    added first/last_name + dob to users table

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,26 @@
 class Api::SessionsController < ApplicationController
+
+  before_action :require_logged_in, only: [:destroy]
+
+  def create 
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if user
+      login(user)
+      render json: 'api/user/show' # should this be users/?
+    else
+      render json: ['Incorrect or incomplete credentials.'], status: 401
+    end
+  end
+
+  def destroy 
+    if logged_in?
+      logout
+      render json: {}
+    else
+      render json: {}, status: 404
+    end
+  end
 end
