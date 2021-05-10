a3a850aba913ce9494b24cf5bf75daf8805917e9
michelleroos <hello.michelleroos@gmail.com>
2021-05-10 14:05:39 -0400

    Created a schema, started workin on user auth controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# == Route Map
+#
+#                    Prefix Verb   URI Pattern              Controller#Action
+#                      root GET    /                        static_pages#root
+#                 api_users POST   /api/users(.:format)     api/users#create {:format=>:json}
+#               api_session DELETE /api/session(.:format)   api/sessions#destroy {:format=>:json}
+#                           POST   /api/session(.:format)   api/sessions#create {:format=>:json}
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
+
 end
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { login, logout, signup } from "./util/session_api_util";
+// import { login, logout, signup } from "./util/session_api_util";
 
 
 
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const test = <div>This is the DOM</div>;
 
-    window.login = login;
-    window.logout = logout;
-    window.signup = signup;
+    // window.login = login;
+    // window.logout = logout;
+    // window.signup = signup;
 
 
     ReactDOM.render(test, root);
