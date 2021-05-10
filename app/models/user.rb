0e92499d4828c780721a1dfb330a0fad6506428d
michelleroos <hello.michelleroos@gmail.com>
2021-05-10 15:13:03 -0400

    updated the gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
     attr_reader :password
     after_initialize :ensure_session_token
 
-    # FIGVAPER
-
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         if user && user.is_password?(password)
