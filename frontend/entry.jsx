beb3d8464ecde010bcf745766c618fe84f95cd89
michelleroos <hello.michelleroos@gmail.com>
2021-05-11 14:58:33 -0400

    imported store to entry

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { logIn, logOut, signUp } from "./actions/session_actions";
 // import { logIn, logOut, signUp } from "./util/session_api_util";
-// import configureStore from "./store/store";
+import configureStore from "./store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
