8b0d0e213ab2e915146e34ab166f72294f1b9103
michelleroos <hello.michelleroos@gmail.com>
2021-05-11 15:05:23 -0400

    reconfigured entry file, app and root components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,9 @@
-// import React from "react";
+import React from "react";
 
-// const App = () => (
-//     <div>
-//         This is the app
-//     </div>
-// );
+const App = () => (
+    <div>
+        This is the app
+    </div>
+);
 
-// export default App;
+export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+
+// TESTING -- TO BE REMOVED
 import { logIn, logOut, signUp } from "./actions/session_actions";
 // import { logIn, logOut, signUp } from "./util/session_api_util";
-import configureStore from "./store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
+
+    // TESTING START
     window.store = store;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    
-    const root = document.getElementById('root');
-    const test = <div>This is the DOM</div>;
-    const image = <div><img src={window.cannabisURL} /></div>
-    
 
-    // TAC
+    // const test = <div>This is the DOM</div>;
+    // const image = <div><img src={window.cannabisURL} /></div>
+    
     window.signUp = signUp;
     window.logIn = logIn;
     window.logOut = logOut;
+    // TESTING END
 
-    // ReactDOM.render(<Root store={store} />, root);
-    ReactDOM.render(image, root);
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={store} />, root);
+    // ReactDOM.render(image, root);
 })
