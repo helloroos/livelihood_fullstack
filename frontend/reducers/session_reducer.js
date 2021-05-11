9b32b187ff9275606a93b235ded6598bb3543fe2
michelleroos <hello.michelleroos@gmail.com>
2021-05-11 12:04:12 -0400

    Prepare for initial Heroku push

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -7,11 +7,11 @@ const _nullSession = { currentUser: null }
 
 const sessionReducer = (state = _nullSession, action) => {
     Object.freeze(state);
-    let newState = Object.assign({}, state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            
+            return Object.assign({}, { currentUser: action.currentUser });
         case LOGOUT_CURRENT_USER:
+            return _nullSession;
         default:
             return state;
     }
