d8d890496b462d5361406a143c9651af070a480b
michelleroos <hello.michelleroos@gmail.com>
2021-09-22 17:00:13 -0400

    sign up styling done

--- frontend/components/session/sign_up_form.jsx
+++ frontend/components/session/sign_up_form.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
-export default function sign_up_form({ logIn, resetErrors, errors }) {
+export default function sign_up_form({ signUp, resetErrors, errors }) {
 
   useEffect(() => {
     document.title = `Sign up | Robinhodl`;
@@ -11,7 +11,14 @@ export default function sign_up_form({ logIn, resetErrors, errors }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    logIn(user)
+    signUp(user)
+    .then (() => {
+      window.location.href = "/";
+    })
+  }
+
+  const update = (e) => {
+    setUser({ ...user, first_name: e.target.value })
   }
 
   return (
@@ -24,7 +31,6 @@ export default function sign_up_form({ logIn, resetErrors, errors }) {
               <Link to="/portfolio">
                 <h2>Robinhodl</h2>
                 <img src={window.robinhood_favicon_black} />
-                {/* <i class="fas fa-hand-holding-usd"></i> */}
               </Link>
             </div>
             <h3 id="second-h3">Make Your Money Move</h3>
@@ -36,16 +42,16 @@ export default function sign_up_form({ logIn, resetErrors, errors }) {
             <p>Please enter your full legal name. Your legal
               name should match any form of government ID.</p>
             <label>
-              <input type="text" className="short" placeholder="First name" onChange={(e) => setUser({ ...user, first_name: e.target.value })} value={user.first_name} required />
+              <input type="text" className="short" placeholder="First name" onChange={update} value={user.first_name} required />
             </label>
             <label>
-              <input type="text" className="short" id="second-input" placeholder="Last name" onChange={(e) => setUser({ ...user, last_name: e.target.value })} value={user.last_name} required />
+              <input type="text" className="short" id="second-input" placeholder="Last name" onChange={update} value={user.last_name} required />
             </label>
             <label>
-              <input type="email" placeholder="Email" onChange={(e) => setUser({ ...user, email: e.target.value })} value={user.email} required />
+              <input type="email" placeholder="Email" onChange={update} value={user.email} required />
             </label>
             <label>
-              <input type="password" placeholder="Password (min. 6 characters)" onChange={(e) => setUser({ ...user, password: e.target.value })} value={user.password} required />
+              <input type="password" placeholder="Password (min. 6 characters)" onChange={update} value={user.password} required />
             </label>
 
             <div className="session-btns" id="sign-up-btn-container">
@@ -54,30 +60,41 @@ export default function sign_up_form({ logIn, resetErrors, errors }) {
               {/* {this.renderErrors()} */}
             </div>
           </form>
+          <div id="fine-print">
+            <p>Robinhodl investments do not involve risk, including the possible loss of principal. Investors do not need to consider their investment objectives and risks carefully before investing.</p>
+            <p>Commission-free trading means $0 commission trading on self-directed individual cash or margin brokerage accounts that trade U.S. listed crytp currencies via mobile devices or web. Keep in mind, other fees such as trading (non-commission) fees, Gold subscription fees, wire transfer fees, and paper statement fees do not apply to your brokerage account. Please see Robinhodl’s fee schedule to learn more.</p>
+            <p>Crypto currencies offered through Robinhodl, Inc.</p>
+            <div id="contact-info">
+              <a href="https://github.com/michelleroos" id="github" target="_blank" className="contact-links">GitHub</a>
+              <a href="https://www.linkedin.com/in/roosmichelle/" id="linkedin" target="_blank" className="contact-links">LinkedIn</a>
+              {/* <a href="#" id="portfolio" className="contact-links" target="_blank">Portfolio</a> */}
+              <a href="https://firebasestorage.googleapis.com/v0/b/scroople-25727.appspot.com/o/Michelle%20Roos%20-%20Resume.pdf?alt=media&token=04dc0414-21f1-4d24-af4e-5a49eceb3371"
+                id="resume" className="contact-links" target="_blank" download><i className="far fa-file"></i></a>
+            </div>
+            <p>© 2021 Michelle Roos. All rights reserved.</p>
+          </div>
         </div>
       </div>
 
-      <div className="info-container">
-        <section>
-          <div className="info">
-            <h3>Commission-free trading</h3>
-            <p>Break free from commission-fees and make
-              unlimited commission-free trades in stocks,
-              funds, and options with Robinhood Financial.
-              Other fees may apply. View our fee schedule to
-              learn more.</p>
-            <h3>Account Protection</h3>
-            <p>Robinhood Financial is a member of SIPC.
-              Securities in your account protected up to
-              $500,000. For details, please see
-              www.sipc.org.</p>
-            <h3>Stay on top of your portfolio</h3>
-            <p>Set up customized news and notifications to stay
-              on top of your assets as casually or as
-              relentlessly as you like. Controlling the
-              flow of info is up to you.</p>
-          </div>
-        </section>
+      <div id="info-container">
+        <div id="info-content">
+          <h3>Commission-free trading</h3>
+          <p>Break free from commission-fees and make
+            unlimited commission-free trades in stocks,
+            funds, and options with Robinhood Financial.
+            Other fees may apply. View our fee schedule to
+            learn more.</p>
+          <h3>Account Protection</h3>
+          <p>Robinhood Financial is a member of SIPC.
+            Securities in your account protected up to
+            $500,000. For details, please see
+            www.sipc.org.</p>
+          <h3>Stay on top of your portfolio</h3>
+          <p>Set up customized news and notifications to stay
+            on top of your assets as casually or as
+            relentlessly as you like. Controlling the
+            flow of info is up to you.</p>
+        </div>
       </div>
     </div>
   )
