e9ca215026d195e3fc9b6d18d6233bb28e947293
michelleroos <hello.michelleroos@gmail.com>
2021-09-24 11:29:05 -0400

    learn more resume download

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -43,7 +43,8 @@ export default function splash() {
         </div>
 
         <div id="splash2-container">
-          <p>See our fee schedule to learn more about cost.</p>
+          <p>See my <a href="https://firebasestorage.googleapis.com/v0/b/scroople-25727.appspot.com/o/Michelle%20Roos%20-%20Resume.pdf?alt=media&token=04dc0414-21f1-4d24-af4e-5a49eceb3371"
+            id="resume" class="header-links" target="_blank" download>resume</a> to learn more about me.</p>
         </div>
 
         <div id="splash3-container">
