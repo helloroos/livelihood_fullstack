8fc18024530df4638d2f57e1893aacd50c294ac6
michelleroos <hello.michelleroos@gmail.com>
2021-09-24 13:10:09 -0400

    splash 4

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,6 +4,7 @@ import DisclosureModal from './disclosure_modal';
 import Splash1 from './splash1';
 import Splash2 from './splash2';
 import Splash3 from './splash3';
+import Splash4 from './splash4';
 
 export default function splash() {
 
@@ -30,39 +31,7 @@ export default function splash() {
 
         <Splash3 isOpen={isOpen}></Splash3>
 
-        <div id="splash4-container">
-          <div id="splash4-text-container">
-            <div id="splash4-header">
-              <h2>Introducing Fractional Shares</h2>
-              <p>Invest in thousands of stocks with as little as $1.</p>
-            </div>
-
-            <div id="splash4-sections-container">
-              <div id="splash4-sec1" className="splash4-sections">
-                <h3>Invest Any Amount</h3>
-                <p>Choose how much you want to invest, and we’ll convert from dollars to parts of a whole share.</p>
-              </div>
-              <div id="splash4-sec2" className="splash4-sections">
-                <h3>Build a Balanced Portfolio</h3>
-                <p>Customize your portfolio with pieces of different companies and funds to help reduce risk.</p>
-              </div>
-              <div id="splash4-sec3" className="splash4-sections">
-                <h3>Trade in Real Time</h3>
-                <p>Trades placed during market hours are executed at that time, so you’ll always know the share price.</p>
-              </div>
-            </div>
-
-            <div id="splash4-disclosure" className="disclosures">
-              <button onClick={() => setIsOpen(true)}>ⓘ Fractional Shares Disclosure</button>
-            </div>
-            <DisclosureModal open={isOpen} onClose={() => setIsOpen(false)}></DisclosureModal>
-          </div>
-
-          <div id="splash4-img-container">
-            <img src={window.frac_shares} alt="fractional_shares" />
-          </div>
-
-        </div>
+        <Splash4 isOpen={isOpen}></Splash4>
 
         <div id="splash5-container">
           <div id="splash5-buttons">
