c305bbb5b82ebcb5eeac2661c6c1c8b7c28ba3a5
michelleroos <hello.michelleroos@gmail.com>
2021-09-24 10:07:40 -0400

    splash 6

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -141,6 +141,32 @@ export default function splash() {
           </div>
         </div>
 
+        <div id="splash6-container">
+          <div id="splash6-inner-container">
+            <div id="splash6-text">
+              <h2>Our Products</h2>
+              <div id="splash6-buttons">
+                <button>Cash Management</button>
+                <button>Stocks & Funds</button>
+                <button>Options</button>
+                <button>Gold</button>
+                <button>Crypto</button>
+              </div>
+              <div id="splash6-info">
+                <p>Earn 0.30% APY* on your uninvested cash and get more flexibility with your brokerage account.</p>
+              </div>
+              <button id="learn">Learn about Cash Management</button>
+              <div id="splash6-disclosure">
+                <a href="#">ⓘ Variable APY and Debit Card Disclosures</a>
+              </div>
+
+            </div>
+            <div id="splash6-img">
+              <img src="https://robinhood.com/us/en/_next/static/images/1x__ea42f9545c170fe8354ebde19dcefbc0.png" alt="cash-mgmt" />
+            </div>
+          </div>
+        </div>
+
 
         {/* <div className="superbowl-campaign">
             <video src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4"></video>
