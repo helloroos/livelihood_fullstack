46a47558d81890d2aa3e9212be2935112fa0acdb
michelleroos <hello.michelleroos@gmail.com>
2021-09-24 09:30:15 -0400

    splash5

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -122,7 +122,23 @@ export default function splash() {
         </div>
 
         <div id="splash5-container">
+          <div id="splash5-buttons">
+            <button>Learn</button>
+            <button>Manage</button>
+            <button>Customize</button>
+          </div>
 
+          <div id="splash5-img">
+            <img src={window.learn} alt="learn" />
+          </div>
+
+          <div id="splash5-text-container">
+
+            <div id="splash5-text">
+              <h1>Learn As You Grow</h1>
+              <p>Our goal is to make investing in financial markets more affordable, more intuitive, and more fun, no matter how much experience you have (or don’t have).</p>
+            </div>
+          </div>
         </div>
 
 
