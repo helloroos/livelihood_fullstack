efee2dd3a3b50d564f7fcd971d2bb55162f2e59e
michelleroos <hello.michelleroos@gmail.com>
2021-10-05 15:28:16 -0400

    plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
 const path = require('path');
 
 module.exports = {
-    context: __dirname,
-    entry: './frontend/entry.jsx',
-    output: {
-        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '*'],
-        fallback: { 
-            "querystring": require.resolve("querystring-es3"),
-            "https": require.resolve("https-browserify"),
-            "url": require.resolve("url/"),
-            "http": require.resolve("stream-http"),
-            "buffer": require.resolve("buffer/")
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/env', '@babel/react']
-                    }
-                },
-            }
-        ]
-    },
-    devtool: 'source-map'
+  context: __dirname,
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+    fallback: {
+      "querystring": require.resolve("querystring-es3"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/"),
+      "http": require.resolve("stream-http"),
+      "buffer": require.resolve("buffer/")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed) // it will automatically pick up key values from .env file
+    })
+  ]
 };
