75e11fea17424e09a6789e1e1d64cbb75c04ecc2
michelleroos <hello.michelleroos@gmail.com>
2022-02-02 20:24:27 -0500

    sign up

--- frontend/components/session/sign_up_form.jsx
+++ frontend/components/session/sign_up_form.jsx
@@ -36,14 +36,12 @@ export default function sign_up_form() {
                 <img src={window.robinhood_favicon_black} />
               </Link>
             </div>
-            <h3 id="second-h3">Make Your Money Move</h3>
-            <p id="first-p">Robinhood lets you invest in companies you love,
-              commission-free.</p>
+            <h3 id="second-h3">Make Your Fake Money Move</h3>
+            <p id="first-p">Robinhodl lets you invest in the crypto currency you love.</p>
           </header>
 
           <form onSubmit={handleSubmit}>
-            <p>Please enter your full legal name. Your legal
-              name should match any form of government ID.</p>
+            <p>Please enter your full name.</p>
             <label>
               <input type="text" className="short" placeholder="First name" onChange={(e) => setUser({ ...user, first_name: e.target.value })} value={user.first_name} required />
             </label>
@@ -65,8 +63,8 @@ export default function sign_up_form() {
           </form>
 
           <div id="fine-print">
-            <p>Robinhodl investments do not involve risk, including the possible loss of principal. Investors do not need to consider their investment objectives and risks carefully before investing.</p>
-            <p>Commission-free trading means $0 commission trading on self-directed individual cash or margin brokerage accounts that trade U.S. listed crytp currencies via mobile devices or web. Keep in mind, other fees such as trading (non-commission) fees, Gold subscription fees, wire transfer fees, and paper statement fees do not apply to your brokerage account. Please see Robinhodl’s fee schedule to learn more.</p>
+            <p>Robinhodl investments do not involve risk, including the possible loss of principal. Users do not need to consider their investment objectives and risks carefully before investing (because it's not ACTUALLY a trading platform).</p>
+            <p>Trading on this platform is not real and does not have any commission. Real time data is pulled in from Coin Gecko to enhance the experience of fake trades. Please see Robinhodl’s fee schedule to learn more.</p>
             <p>Crypto currencies offered through Robinhodl, Inc.</p>
             <div id="contact-info">
               <a href="https://github.com/michelleroos" id="github" target="_blank" className="contact-links">GitHub</a>
@@ -82,19 +80,15 @@ export default function sign_up_form() {
 
       <div id="info-container">
         <div id="info-content">
-          <h3>Commission-free trading</h3>
+          <h3>Free trading with fake money</h3>
           <p>Break free from commission-fees and make
-            unlimited commission-free trades in stocks,
-            funds, and options with Robinhood Financial.
-            Other fees may apply. View our fee schedule to
+            unlimited commission-free trades in fake currencies through Robinhodl.
+            No fees may apply. View our fee schedule to
             learn more.</p>
           <h3>Account Protection</h3>
-          <p>Robinhood Financial is a member of SIPC.
-            Securities in your account protected up to
-            $500,000. For details, please see
-            www.sipc.org.</p>
+          <p>Since this is not a real platform, there is no financial risk.</p>
           <h3>Stay on top of your portfolio</h3>
-          <p>Set up customized news and notifications to stay
+          <p>Set up customized news to stay
             on top of your assets as casually or as
             relentlessly as you like. Controlling the
             flow of info is up to you.</p>
@@ -103,134 +97,3 @@ export default function sign_up_form() {
     </div>
   )
 }
-
-// class SignUpForm extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       email: "",
-//       password: "",
-//       first_name: "",
-//       last_name: ""
-//     }
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     this.update = this.update.bind(this);
-//   }
-
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     const user = Object.assign({}, this.state);
-//     this.props.signUp(user)
-//       .then(() => {
-//         this.props.history.push('/portfolio')
-//       })
-//       .then(() => {
-//         this.props.resetErrors
-//       })
-//   }
-
-//   update(field) {
-//     return (e) => this.setState({
-//       [field]: e.currentTarget.value
-//     });
-//   }
-
-//   // renderErrors() {
-//   //     return (
-//   //         <ul>
-//   //             {this.props.errors.map((error, i) => (
-//   //                 <li key={`error-${i}`}>
-//   //                     {error}
-//   //                 </li>
-//   //             ))}
-//   //         </ul>
-//   //     );
-//   // }
-
-//   render() {
-//     return (
-//       <div className="sign-up-container">
-//         <section className="form-container">
-//           <div className="form">
-//             <header>
-//               <h3>Robinhodl</h3>
-//               <h3>Make Your Money Move</h3>
-//               <p>Robinhood lets you invest in companies you love,
-//                 commission-free.</p>
-//             </header>
-//             <br />
-//             <form onSubmit={this.handleSubmit}>
-//               <p>Please enter your full legal name. Your legal
-//                 name should match any form of government ID.</p>
-//               <label>
-//                 <input
-//                   type="text"
-//                   id="short"
-//                   placeholder="First name"
-//                   value={this.state.first_name}
-//                   onChange={this.update('first_name')}
-//                   required />
-//               </label>
-//               <label>
-//                 <input
-//                   type="text"
-//                   id="short"
-//                   placeholder="Last name"
-//                   value={this.state.last_name}
-//                   onChange={this.update('last_name')}
-//                   required />
-//               </label>
-//               <label>
-//                 <input
-//                   type="email"
-//                   id="long"
-//                   placeholder="Email"
-//                   value={this.state.email}
-//                   onChange={this.update('email')}
-//                   required />
-//               </label>
-//               <br />
-//               <label>
-//                 <input
-//                   type="password"
-//                   id="long"
-//                   placeholder="Password (min. 6 characters)"
-//                   value={this.state.password}
-//                   onChange={this.update('password')}
-//                   required />
-//               </label>
-//               <br />
-//               <p>Already a user? Log in instead.</p>
-//               {/* {this.renderErrors()} */}
-//               <button type="submit">{this.props.formType}</button>
-//             </form>
-//           </div>
-//         </section>
-//         <div className="info-container">
-//           <section>
-//             <div className="info">
-//               <h3>Commission-free trading</h3>
-//               <p>Break free from commission-fees and make
-//                 unlimited commission-free trades in stocks,
-//                 funds, and options with Robinhood Financial.
-//                 Other fees may apply. View our fee schedule to
-//                 learn more.</p>
-//               <h3>Account Protection</h3>
-//               <p>Robinhood Financial is a member of SIPC.
-//                 Securities in your account protected up to
-//                 $500,000. For details, please see
-//                 www.sipc.org.</p>
-//               <h3>Stay on top of your portfolio</h3>
-//               <p>Set up customized news and notifications to stay
-//                 on top of your assets as casually or as
-//                 relentlessly as you like. Controlling the
-//                 flow of info is up to you.</p>
-//             </div>
-//           </section>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default SignUpForm;
